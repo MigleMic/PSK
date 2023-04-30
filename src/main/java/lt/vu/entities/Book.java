7348b045631a1120975987b46de943d7cfd32ad0
MigleMic <anaztazija2@gmail.com>
2023-04-30 15:07:18 +0300

    Task 2 update

--- src/main/java/lt/vu/entities/Book.java
+++ src/main/java/lt/vu/entities/Book.java
@@ -34,6 +34,9 @@ public class Book implements Serializable
             inverseJoinColumns = @JoinColumn(name = "GENRE_ID"))
     private List<Genre> genres = new ArrayList<>();
 
+    @Version
+    @Column(name = "VERSION")
+    private Integer version;
     @Override
     public int hashCode()
     {
